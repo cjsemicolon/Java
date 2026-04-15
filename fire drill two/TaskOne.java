2fa81dbfa260b0073463df9735ef1a64f2992951
cjsemicolon <cjsemicolon@gmail.com>
2026-04-15 17:01:50 +0100

    fire drill

---
+++ fire drill two/TaskOne.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class TaskOne {
+
+    public static void main(String [] args){
+
+        Scanner userInput = new Scanner(System.in);
+         
+        int sum = 0;  
+       int  scores = 0   ;  
+ 
+        
+        for (scores = 1; scores <=10; scores++) {
+
+          
+                System.out.print("Enter value " + score);
+            int score = userInput.nextInt();
+
+                
+            
+            }       
+        
+
+        sum = sum + scores;
+
+        System.out.print(sum);
+
+        
+
+      
+
+    }    
+
+}
